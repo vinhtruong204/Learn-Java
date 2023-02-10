535b63590095ba5dc9ef215771332c5d9a1f9fcb
Ving <vt3839796@gmail.com>
2023-02-10 15:56:06 +0700

    Edit XuLyChuoi3.java file

--- XuLyChuoi3.java
+++ XuLyChuoi3.java
@@ -1,5 +1,7 @@
 public class XuLyChuoi3 {
     public static void main(String[] args) {
-        String s1 = "Xin chao ";
+        String s1 = "Xin chao ong, xin chao ba, xin chao co, xin chao chu";
+        String s2 = "Xin chao";
+        
     }
 }
