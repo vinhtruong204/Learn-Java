85f9907ce2489f83804c57c8b05b7212b4192a34
Ving <vt3839796@gmail.com>
2023-02-12 20:18:43 +0700

    Rename XuLyChuoi3.java file

--- TimKiemChuoi.java
+++ TimKiemChuoi.java
@@ -1,4 +1,4 @@
-public class XuLyChuoi3 {
+public class TimKiemChuoi {
     public static void main(String[] args) {
         String s1 = "Xin chao ong, xin chao ba, xin chao co, xin chao chu";
         String s2 = "Xin chao";
